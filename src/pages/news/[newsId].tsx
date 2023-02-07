b9a74c14c23290907f70f8265ce2471ecbd78fe5
Julian <julian890701@gmail.com>
2023-02-07 16:09:56 +0800

    Add news pages

---
+++ src/pages/news/[newsId].tsx
@@ -0,0 +1,4 @@
+function NewsDetailPage() {
+  return <h1>This is News detail page</h1>;
+}
+export default NewsDetailPage;
---
+++ src/pages/news/index.tsx
@@ -0,0 +1,4 @@
+function AllNewsPage() {
+  return <h1>This is All news page</h1>;
+}
+export default AllNewsPage;
