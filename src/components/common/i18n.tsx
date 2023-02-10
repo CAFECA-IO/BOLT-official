f58f35d360c5e6332d4d4fc4102aab10b24da849
Julian <julian890701@gmail.com>
2023-02-10 18:01:35 +0800

    Develop i18n css

--- src/components/common/i18n.tsx
+++ src/components/common/i18n.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Image from "next/image";
 import myStyles from "@/styles/navbar.module.css";
 
 function I18n() {
+  //collapse選單收合
+  const [showSubMenu, setShowSubMenu] = useState(false);
+  const clickHandler = () => {
+    setShowSubMenu(!showSubMenu);
+  };
+  const subMenuStyles = showSubMenu
+    ? myStyles.langmenu_open
+    : myStyles.langmenu_close;
+
   return (
     <>
-      <div className={myStyles.lang_icon_box}>
+      <div className={myStyles.lang_icon_box} onClick={clickHandler}>
         <Image alt="" src="./img/icons/globe.svg" width={44} height={44} />
       </div>
 
-      <div className={myStyles.lang_dropmenu}>
-        <ul>
+      <div className={subMenuStyles}>
+        <ul className={myStyles.lang_dropmenu}>
           <li>English</li>
           <li>繁體中文</li>
           <li></li>
