613409a8fc2bfa26b31d0988ad0f290bb7a82a2e
Julian <julian890701@gmail.com>
2023-02-10 17:44:01 +0800

    Develop i18n component

--- src/components/common/i18n.tsx
+++ src/components/common/i18n.tsx
@@ -1,4 +1,21 @@
+import Image from "next/image";
+import myStyles from "@/styles/navbar.module.css";
+
 function I18n() {
-  return <></>;
+  return (
+    <>
+      <div className={myStyles.lang_icon_box}>
+        <Image alt="" src="./img/icons/globe.svg" width={44} height={44} />
+      </div>
+
+      <div className={myStyles.lang_dropmenu}>
+        <ul>
+          <li>English</li>
+          <li>繁體中文</li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
 }
 export default I18n;
