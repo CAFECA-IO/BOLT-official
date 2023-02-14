98bd1790694907a0f8bfb988da0a21c1ac78d229
Julian <julian890701@gmail.com>
2023-02-14 18:04:37 +0800

    Develop contact_us_form component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import animationData from "../../public/animation/decentralized.json";
 import myStyles from "@/styles/Home.module.css";
 import BoltMain from "@/components/home/bolt_main";
 import BoltIntro from "@/components/home/bolt_introduction";
+import ContactUsForm from "@/components/common/contact_us_form";
 
 export default function Home() {
   const { t } = useTranslation("common");
@@ -36,28 +37,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" /> {/* 加入書籤列看到的小icon */}
       </Head>
       <main className={myStyles.main}>
-        <div className={myStyles.main_block1}>
-          <BoltMain />
+        <div className={myStyles.main_bg}>
+          <div className={myStyles.main_block1}>
+            <BoltMain />
 
-          <Link href="/#what" scroll={false} className={myStyles.scroll_down}>
-            <Image
-              src="/img/icons/scroll_down.png"
-              alt="scroll_down"
-              width={20}
-              height={20}
-            />
-          </Link>
+            <Link href="/#what" scroll={false} className={myStyles.scroll_down}>
+              <Image
+                src="/img/icons/scroll_down.png"
+                alt="scroll_down"
+                width={20}
+                height={20}
+              />
+            </Link>
+          </div>
+          <div id="what" className={myStyles.main_block2}>
+            <h1>{t("main.block2.title")}</h1>
+            <BoltIntro />
+          </div>
+          <div id="safe" className={myStyles.main_block3}>
+            <h1>{t("main.block3.title")}</h1>
+            <div
+              style={{ height: "775px", width: "624px" }}
+              ref={animContainer}
+            ></div>
+          </div>
         </div>
-        <div id="what" className={myStyles.main_block2}>
-          <h1>{t("main.block2.title")}</h1>
-          <BoltIntro />
-        </div>
-        <div id="safe" className={myStyles.main_block3}>
-          <h1>{t("main.block3.title")}</h1>
-          <div
-            style={{ height: "775px", width: "624px" }}
-            ref={animContainer}
-          ></div>
+
+        <div id="contact_us" className={myStyles.main_block4}>
+          <h1>{t("contactForm.title")}</h1>
+          <ContactUsForm />
         </div>
       </main>
     </>
