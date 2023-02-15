69251bfb61965d32ee578615d36bad1f4c07147c
Julian <julian890701@gmail.com>
2023-02-15 11:28:08 +0800

    Why bolt is done

--- src/pages/about_bolt.tsx
+++ src/pages/about_bolt.tsx
@@ -1,5 +1,28 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import myStyles from "@/styles/about.module.css";
+import WhyBolt from "@/components/about/why_bolt";
+
 function AboutBoltPage() {
-  return <h1>This is About Bolt Page</h1>;
+  const { t } = useTranslation("common");
+
+  return (
+    <div className={myStyles.about_container}>
+      <div className={myStyles.about_block1}>
+        <h1>{t("about.whyBolt.title")}</h1>
+        <WhyBolt />
+      </div>
+    </div>
+  );
 }
 
 export default AboutBoltPage;
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
