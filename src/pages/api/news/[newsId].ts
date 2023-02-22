6a772981527643f1d22493729ac2f67af16a7c9d
Julian <julian890701@gmail.com>
2023-02-22 15:13:35 +0800

    Privacy is done

--- src/pages/api/news/[newsId].ts
+++ src/pages/api/news/[newsId].ts
@@ -1,8 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getNewsById } from "@/interfaces/news_detail";
+import { dummyNewsData } from "@/interfaces/news_detail";
+//import { getNewsById } from "@/interfaces/news_detail";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { newsId } = req.query;
+
+  function getNewsById(id: string) {
+    const news = dummyNewsData.find((news) => news.id === id);
+    return news || null;
+  }
 
   const news = getNewsById(newsId as string);
 
