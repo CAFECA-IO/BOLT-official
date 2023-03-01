e7295ba14e065d2ad322a082b990528ed10074fd
Julian <julian890701@gmail.com>
2023-03-01 15:20:55 +0800

    Navbar mobile is done

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { useTranslation } from "next-i18next";
@@ -19,53 +18,46 @@ function MainNavbar() {
     : myStyles.menu_btn_burger;
 
   return (
-    <>
-      <nav className={myStyles.nav_main_container}>
-        <div>
-          <Link href="/#">
-            <Image
-              alt="bolt_logo"
-              src="/img/bolt_logo_main.svg"
-              width={160}
-              height={45}
-            />
-          </Link>
-        </div>
+    <nav className={myStyles.nav_main_container}>
+      <div className={myStyles.nav_logoArea}>
+        <Link href="/#">
+          <div className={myStyles.nav_logo}></div>
+        </Link>
 
         <label onClick={navToggler} className={myStyles.menu_btn}>
           <div className={btnStyles}></div>
           <div className={btnStyles}></div>
           <div className={btnStyles}></div>
         </label>
+      </div>
 
-        <div className={`${menuStyles} ${myStyles.nav_list_container}`}>
-          <ul className={myStyles.leftList}>
-            <li>
-              <Link href="/about_bolt">{t("nav.about")}</Link>
-            </li>
-            <li>
-              <Link href="/technology">{t("nav.technology")}</Link>
-            </li>
-            <li>
-              <Link href="/faq">{t("nav.faq")}</Link>
-            </li>
-          </ul>
+      <div className={`${menuStyles} ${myStyles.nav_list_container}`}>
+        <ul className={myStyles.leftList}>
+          <li>
+            <Link href="/about_bolt">{t("nav.about")}</Link>
+          </li>
+          <li>
+            <Link href="/technology">{t("nav.technology")}</Link>
+          </li>
+          <li>
+            <Link href="/faq">{t("nav.faq")}</Link>
+          </li>
+        </ul>
 
-          <span></span>
+        <span></span>
 
-          <ul className={myStyles.rightList}>
-            <li>
-              <I18n />
-            </li>
-            <li>
-              <Link href="" className={myStyles.blockchain_btn}>
-                {t("nav.blockchain")}
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+        <ul className={myStyles.rightList}>
+          <li>
+            <I18n />
+          </li>
+          <li>
+            <Link href="" className={myStyles.blockchain_btn}>
+              {t("nav.blockchain")}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 export default MainNavbar;
