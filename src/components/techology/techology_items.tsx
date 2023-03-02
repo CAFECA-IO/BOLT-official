39b50877f9d3177b736e1300a263994d40753ece
Julian <julian890701@gmail.com>
2023-03-02 16:51:19 +0800

    Techology page mobile is done

--- src/components/techology/techology_items.tsx
+++ src/components/techology/techology_items.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useTranslation } from "next-i18next";
 import myStyles from "@/styles/technology.module.css";
 
@@ -20,7 +20,7 @@ function TechnologyItems({
   return (
     <div id={anchor} className={myStyles.tech_item_container}>
       <div className={myStyles.tech_item_imgbox}>
-        <Image src={image} alt={t(title)} width={400} height={400} />
+        <Image src={image} layout="fill" objectFit="contain" />
       </div>
       <div className={myStyles.tech_item_textbox}>
         <h4>{t(title)}</h4>
